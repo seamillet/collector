d6acafd88a73d2336756f6d701572a151ea42321
Will Cheng <will.cheng@smartteamglb.com>
2017-10-17 21:17:15 +0800

    merge

---
+++ app/src/main/java/com/willc/collector/lib/HouseLayer.java
@@ -0,0 +1,11 @@
+package com.willc.collector.lib;
+
+import srs.Layer.Layer;
+import srs.Layer.ILayer;
+
+/**
+ * Created by stg on 17/10/16.
+ */
+
+public class HouseLayer extends Layer implements ILayer {
+}
